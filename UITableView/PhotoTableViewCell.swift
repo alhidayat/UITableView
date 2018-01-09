db2d610618a122f31dfa477ff73d86c3e84e3918
alhidayat <al.hidayat23@gmail.com>
2018-01-09 08:54:18 +0700

    UITableView

---
+++ UITableView/PhotoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoTableViewCell.swift
+//  UITableView
+//
+//  Created by Alhidayat on 1/8/18.
+//  Copyright © 2018 Alhidayat. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ UITableView/Photos.swift
@@ -0,0 +1,9 @@
+//
+//  Photos.swift
+//  UITableView
+//
+//  Created by Alhidayat on 1/8/18.
+//  Copyright © 2018 Alhidayat. All rights reserved.
+//
+
+import Foundation
